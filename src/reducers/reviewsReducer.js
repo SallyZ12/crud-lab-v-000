4b293498c81906282a1d55c6141458b723bd620d
SallyZ12 <szasloff@gmail.com>
2019-07-01 17:44:38 -0400

    fixed return value for Case ADD_REVIEW

--- src/reducers/reviewsReducer.js
+++ src/reducers/reviewsReducer.js
@@ -7,10 +7,10 @@ export default (state=initialState, action)=> {
   switch (action.type){
 
     case "ADD_REVIEW":
-    console.log("rev add text:", action.review.text)
-    console.log("rev add restId:", action.review.restaurantId)
+    // console.log("rev add text:", action.review.text)
+    // console.log("rev add restId:", action.review.restaurantId)
     const review = { id: cuidFn(), text: action.review.text, restaurantId: action.review.restaurantId }
-      return { ...state, reviews: [...state.reviews, review]}
+      return { ...state, reviews: state.reviews.concat(review)}
 
     case "DELETE_REVIEW":
     const reviews = state.reviews.filter(review => review.id !== action.id)
