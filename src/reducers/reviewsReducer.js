7740ee4d7f75e4969b06387b3aae1f67d4274262
SallyZ12 <szasloff@gmail.com>
2019-07-01 19:39:24 -0400

    deleted commented code no longer used in case ADD_REVIEW

--- src/reducers/reviewsReducer.js
+++ src/reducers/reviewsReducer.js
@@ -10,7 +10,6 @@ export default (state=initialState, action)=> {
     // console.log("rev add text:", action.review.text)
     // console.log("rev add restId:", action.review.restaurantId)
     const review = { id: cuidFn(), text: action.review.text, restaurantId: action.review.restaurantId }
-      // return { ...state, reviews: state.reviews.concat(review)}
       return state.concat(review)
 
     case "DELETE_REVIEW":
