0d5bd290979aaf3dcc4705fc9000caf20cafe480
SallyZ12 <szasloff@gmail.com>
2019-07-07 18:37:38 -0400

    In ADD_REDUCER: changed variable text to action.text.text from action.text, changed return, in DELETE_RESTAURANT: for filter changed to state.filter from state.restaurants.filter and changed return to restaurants, and added comments

--- src/reducers/restaurantsReducer.js
+++ src/reducers/restaurantsReducer.js
@@ -2,32 +2,25 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-const initialState =  { restaurants: [ ], text: ""}
+const initialState =  []
+
 
 export default (state=initialState, action) => {
   switch (action.type){
-    case "ADD_RESTAURANT":
-console.log("add_rest:", action)
 
-      const restaurant = { id: cuidFn(), text: action.text }
-        // console.log("reducer add rest id:", restaurant.id)
-        // console.log("add rest:", restaurant)
-        return { ...state, restaurants: state.restaurants.concat(restaurant) }
+    case "ADD_RESTAURANT":
+        const restaurant = {id: cuidFn(), text: action.text.text }
+          return state.concat(restaurant)
 
 // remember to spread on state (...state) so don't delete existing state
 
     case "DELETE_RESTAURANT":
-      // console.log("reducer delete:", action.id)
+        const restaurants = state.filter(restaurant => (restaurant.id !== action.id))
+          return restaurants
 
-        const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
-          return {...state, restaurants}
-
-    case "UPDATE_TEXT":
-    // console.log("up text red:", action.text)
-          return {...state, text: action.text}
-
+// not using this yet, will have to refactor for combinedReducer
     case "UPDATE_RESTAURANT":
-    console.log("up rest red:", action.restaurant)
+    // console.log("up rest red:", action.restaurant)
       const updatedRestaurants = state.restaurants.map(restaurant => {
         if (restaurant.id === action.restaurant.id){
         return action.restaurant
@@ -35,10 +28,6 @@ console.log("add_rest:", action)
         return restaurant
       })
       return {...state, restaurants: updatedRestaurants}
-
-    case "CLEAR_TEXT":
-        return {...state, text: ""}
-  
 
     default:
       return state
